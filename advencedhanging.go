72a12a19eb2cd55f01289d8509040d465b858604
peanthony <anthony.pereira@ynov.com>
2024-01-04 13:01:18 +0100

    removed some exit

--- advencedhanging.go
+++ advencedhanging.go
@@ -41,69 +41,67 @@ func HangmanADV(wordslist string) *HangManData { //return the struct
 
 func Playingadv(hangadv *HangManData, letter []rune) *HangManData {
 	hangadv.VictoryCondition = 0
-	for hangadv.NbrOfAttempt > 0 && hangadv.VictoryCondition == 0 {
-		for loop := 0; loop < len(hangadv.WordToFind); loop++ {
-			fmt.Print(hangadv.WordToFind[loop])
-		}
-		fmt.Print("\n")
-		if len(letter) > 1 {
-			condition := 0
-			alreadyGot := 0
-			if 1 < len(letter) && len(letter) == len(hangadv.Word) {
-				for b := 0; b < len(hangadv.GivenWord); b++ {
-					if string(letter) == hangadv.GivenWord[b] {
-						alreadyGot = 1
-					}
+	for loop := 0; loop < len(hangadv.WordToFind); loop++ {
+		fmt.Print(hangadv.WordToFind[loop])
+	}
+	fmt.Print("\n")
+	if len(letter) > 1 {
+		condition := 0
+		alreadyGot := 0
+		if 1 < len(letter) && len(letter) == len(hangadv.Word) {
+			for b := 0; b < len(hangadv.GivenWord); b++ {
+				if string(letter) == hangadv.GivenWord[b] {
+					alreadyGot = 1
 				}
-				if alreadyGot == 1 {
-					fmt.Println("already attempted word")
-					fmt.Println(hangadv.GivenWord)
-				} else {
-					hangadv.GivenWord = append(hangadv.GivenWord, string(letter))
-					for i := 0; i < len(hangadv.Word); i++ {
-						if letter[i] != hangadv.Word[i] {
-							condition = 1
-						}
-					}
-					if condition == 1 {
-						hangadv.NbrOfAttempt = hangadv.NbrOfAttempt - 2
-					} else {
-						hangadv.VictoryCondition = 1
-					}
-				}
+			}
+			if alreadyGot == 1 {
+				fmt.Println("already attempted word")
+				fmt.Println(hangadv.GivenWord)
 			} else {
-				fmt.Println("attempted word cannot be seeked word (different lenght)")
+				hangadv.GivenWord = append(hangadv.GivenWord, string(letter))
+				for i := 0; i < len(hangadv.Word); i++ {
+					if letter[i] != hangadv.Word[i] {
+						condition = 1
+					}
+				}
+				if condition == 1 {
+					hangadv.NbrOfAttempt = hangadv.NbrOfAttempt - 2
+				} else {
+					hangadv.VictoryCondition = 1
+				}
 			}
 		} else {
-			condition := 0
-			for o := 0; o < len(hangadv.GivenLetter); o++ {
-				if letter[0] == hangadv.GivenLetter[o] {
-					condition = 1
-				}
-			}
-			if condition == 0 {
-				hangadv.GivenLetter = append(hangadv.GivenLetter, letter[0])
-				wrong := 1
-				for i := 0; i < len(hangadv.Word); i++ {
-					if letter[0] == hangadv.Word[i] {
-						hangadv.WordToFind[i] = string(letter[0])
-						wrong = 0
-					}
-				}
-				if wrong == 1 {
-					hangadv.NbrOfAttempt = hangadv.NbrOfAttempt - 1
-				}
-			} else {
-				fmt.Println("letter already tested")
+			fmt.Println("attempted word cannot be seeked word (different lenght)")
+		}
+	} else {
+		condition := 0
+		for o := 0; o < len(hangadv.GivenLetter); o++ {
+			if letter[0] == hangadv.GivenLetter[o] {
+				condition = 1
 			}
 		}
-		if hangadv.VictoryCondition == 0 {
-			// fmt.Println("number of attempt left : ", nbrOfAttempt)
-			hangadv.VictoryCondition = 1 // check if there still is a _ however if a word contain a _ it get block it isn't an issue yet
-			for f := 0; f < len(hangadv.WordToFind); f++ {
-				if hangadv.WordToFind[f] == "_" {
-					hangadv.VictoryCondition = 0
+		if condition == 0 {
+			hangadv.GivenLetter = append(hangadv.GivenLetter, letter[0])
+			wrong := 1
+			for i := 0; i < len(hangadv.Word); i++ {
+				if letter[0] == hangadv.Word[i] {
+					hangadv.WordToFind[i] = string(letter[0])
+					wrong = 0
 				}
+			}
+			if wrong == 1 {
+				hangadv.NbrOfAttempt = hangadv.NbrOfAttempt - 1
+			}
+		} else {
+			fmt.Println("letter already tested")
+		}
+	}
+	if hangadv.VictoryCondition == 0 {
+		// fmt.Println("number of attempt left : ", nbrOfAttempt)
+		hangadv.VictoryCondition = 1 // check if there still is a _ however if a word contain a _ it get block it isn't an issue yet
+		for f := 0; f < len(hangadv.WordToFind); f++ {
+			if hangadv.WordToFind[f] == "_" {
+				hangadv.VictoryCondition = 0
 			}
 		}
 	}
